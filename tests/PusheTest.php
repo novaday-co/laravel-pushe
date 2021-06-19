2b422fdcb6522d93e8a5704241d5da9644f091c1
farid froozan <froozan@yahoo.com>
2021-06-19 20:52:58 +0430

    add test

--- tests/PusheTest.php
+++ tests/PusheTest.php
@@ -7,25 +7,20 @@ use PHPUnit\Framework\TestCase;
 
 class PusheTest extends TestCase
 {
-    public function testSimple()
+    public function testBody()
     {
         $pushe = new Pushe;
-        dd($pushe->setToken('17c8217d375e7fddcb6350e327d57300fa726bc5')
-        ->setAppIds('8ep6lj84yk5103xd')
-        ->setFilters([
+        $response = $pushe->setFilters([
             'tags' => [
                 'user_id' => '123',
-                'environment' => 'development',
-                'server' => 'farazin',
-                'database' => 'dsp_3_1',
             ]
         ])
         ->setData([
-            "title" => "This is a topic notification",
-            "content" => "Only users already subscribed to topic can see me",
-        ])
-        ->send());
- 
-        // $this->assertEquals('https://fa.wikipedia.org/wiki/مدیاویکی:Gadget-Extra-Editbuttons-botworks.js', PersianTools::URLfix('https://fa.wikipedia.org/wiki/%D9%85%D8%AF%DB%8C%D8%A7%D9%88%DB%8C%DA%A9%DB%8C:Gadget-Extra-Editbuttons-botworks.js'));
+            "title" => "This is a filtered notification",
+            "content" => "Only users already subscribed to filter can see me",
+        ]);
+
+        $this->assertEquals($response->filters['tags']['user_id'],123);
+        $this->assertEquals($response->data['title'],"This is a filtered notification");
     }
 }
