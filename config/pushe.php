607561110d1a69064262ca98c3e140183629cf6f
farid froozan <froozan@yahoo.com>
2021-06-19 22:41:41 +0430

    change in config

--- config/pushe.php
+++ config/pushe.php
@@ -2,8 +2,8 @@
 
 return [
     'timeout' => 30,
-    'token' => '17c8217d375e7fddcb6350e327d57300fa726bc5',
-    'app_ids' => ['8ep6lj84yk5103xd'],
+    'token' => '',
+    'app_ids' => [''],
     'retry_attempt' => 3,
     'retry_wait_milliseconds' => 100,
 ];
