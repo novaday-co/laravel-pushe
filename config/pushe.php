0d61482a21d37d2e8dda49f7b2185c0babed423f
farid froozan <froozan@yahoo.com>
2021-06-19 23:56:18 +0430

    update config

--- config/pushe.php
+++ config/pushe.php
@@ -2,8 +2,8 @@
 
 return [
     'timeout' => 30,
-    'token' => '',
-    'app_ids' => [''],
+    'token' =>  env('PUSHE_TOKEN', null),
+    'app_ids' => [env('PUSHE_APP_ID', null)],
     'retry_attempt' => 3,
     'retry_wait_milliseconds' => 100,
 ];
